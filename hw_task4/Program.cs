58e02b0851866c750bd6dcf9f1f268cbb818d0f4
asel_creator <temerbaevatk@gmail.com>
2024-01-08 17:55:08 +0600

    add hw_task4

---
+++ hw_task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N
+// а на выходе показывает его цифры через запятую.
+// 568 => 5,6,8
+// 8 => 8
+// 9542 => 9,5,4,2
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    while (N > 0)
+    {
+        int digit = N % 10;
+        N /= 10;
+        if (N > 0)
+        {
+            Console.Write(digit + ", ");
+        }
+        else
+        {
+            Console.WriteLine(digit);
+        }
+    }
+}
