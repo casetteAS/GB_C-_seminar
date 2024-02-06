edfff33756b865e5852a7187c52ec857389174db
asel_creator <temerbaevatk@gmail.com>
2024-02-07 00:27:23 +0600

    add seminar7:hw_task2

---
+++ hw_task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+int[] arr = [1, 2, 3, 4, 5, 6];
+void PrintElements(int i, int[] array)
+{
+    if (i > array.Length-1) return;
+    PrintElements(i + 1, array);
+    Console.Write($"{array[i]} ");
+}
+PrintElements(0, arr);
