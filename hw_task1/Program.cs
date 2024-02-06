1756ab24bfa07675cc2746988a3f56fc5d301bac
asel_creator <temerbaevatk@gmail.com>
2024-02-07 00:51:11 +0600

    add seminar7:hw_task1

---
+++ hw_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+int Ackerman_Func(int n, int m)
+{
+    if (n == 0) return m + 1;
+    else
+    {
+        if (m == 0) return Ackerman_Func(n-1, 1);
+    }
+    return Ackerman_Func(n - 1, Ackerman_Func(n, m - 1));
+}
+
+Console.Write($"{Ackerman_Func(3, 2)} ");
--- task3/Program.cs
+++ task3/Program.cs
@@ -24,21 +24,3 @@ void ConsonantLetters(int i, string allSymbol)
 Console.WriteLine("Введите строку, содержащую латинские буквы: ");
 string stringAllSymbol = Console.ReadLine();
 ConsonantLetters(0, stringAllSymbol);
-
-// void PrintConsonants(int index, string allSymbol)
-// {
-// if (index > allSymbol.Length - 1)
-// {
-// return;
-// }
-
-// char t = char.ToLower(allSymbol[index]);
-// if (t != 'e' && t != 'a' && t != 'o' && t != 'i' && t != 'u' && t != 'y')
-// {
-// Console.Write($"{allSymbol[index]} ");
-// }
-// PrintConsonants(index + 1, allSymbol);
-// }
-// Console.WriteLine("Введите строку, содержащую латинские буквы");
-// string StringAllSymbol = Console.ReadLine();
-// PrintConsonants(0, StringAllSymbol);
